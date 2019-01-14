3a504e87b4bc13d14e45ea933736c90220c6add7
Rajan Chandi <chandi.rajan@gmail.com>
2019-01-14 14:48:02 +0530

    blog post list ui

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="page-index">
-    <h1>Nuxt Serverless Template {{ this.version }}</h1>
-    <p>{{ message }}</p>
-    <nuxt-link to="/typescript">typescript</nuxt-link>
-    <nuxt-link to="/nuxt">nuxt</nuxt-link>
+    <!-- <h1>Nuxt Serverless Template {{ this.version }}</h1> -->
+    <h1>All Blog Posts </h1>
+    <!-- <p>{{ message }}</p> -->
+    <li v-for="post in posts">
+      <a v-bind:href="'/post/'+ post._id">{{ post.title }}</a>
+    </li><br><br><br>
+    <a href="/post/new">New Post</a>
+<!--     <nuxt-link to="/typescript">typescript</nuxt-link>
+    <nuxt-link to="/nuxt">nuxt</nuxt-link> -->
   </div>
 </template>
 
@@ -19,6 +24,11 @@ export default class PageIndex extends Vue {
   private async asyncData() {
     return {
       message: 'Say Hello to Node.js 8.10 in AWS Lambda and Nuxt 2.0',
+      posts: [
+        {_id: '1', title: 'post one'}, 
+        {_id: '2', title: 'post two'}, 
+        {_id: '3', title: 'post three'}
+      ]
     }
   }
 
